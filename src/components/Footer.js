352e1389afb972edc9492316a07e2c54a7f778aa
lisha <lishacarlo16@gmail.com>
2024-06-23 23:06:24 +0530

    Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,35 +6,43 @@ import EIcon from "../icon/Email.svg";
 import GIcon from "../icon/Github.svg";
 
 function Footer() {
-  return (
-    <footer>
-      <div className="item">
-        <a href="tel:+917347789221">
-          <img src={CIcon} alt="Contact Icon" className="cicon" />
-        </a>{" "}
-      </div>{" "}
-      <div className="item">
-        <a href="mailto:lishacarlo16@gmail.com">
-          <img src={EIcon} alt="Email Icon" className="eicon" />
-        </a>{" "}
-      </div>{" "}
-      <div className="item">
-        <a href="https://www.linkedin.com/in/lisha-carlo-b15a03229">
-          <img src={LIcon} alt="Linkedin Icon" className="licon" />{" "}
-        </a>{" "}
-      </div>{" "}
-      <div className="item">
-        <a href="https://www.github.com/Lishaaaaaa">
-          <img src={GIcon} alt="Github Icon" className="gicon" />{" "}
-        </a>{" "}
-      </div>{" "}
-      <div className="item-copyright">
-        {" "}
-        {String.fromCharCode(169)} {new Date().getFullYear()} Lisha.All rights
-        reserved.{" "}
-      </div>{" "}
-    </footer>
-  );
+    return ( <
+        footer >
+
+        <
+        div className = "item" >
+        <
+        a href = "mailto:lishacarlo16@gmail.com" >
+        <
+        img src = { EIcon }
+        alt = "Email Icon"
+        className = "eicon" / >
+        <
+        /a>{" "} <
+        /div>{" "} <
+        div className = "item" >
+        <
+        a href = "https://www.linkedin.com/in/lisha-carlo-b15a03229" >
+        <
+        img src = { LIcon }
+        alt = "Linkedin Icon"
+        className = "licon" / > { " " } <
+        /a>{" "} <
+        /div>{" "} <
+        div className = "item" >
+        <
+        a href = "https://www.github.com/Lishaaaaaa" >
+        <
+        img src = { GIcon }
+        alt = "Github Icon"
+        className = "gicon" / > { " " } <
+        /a>{" "} <
+        /div>{" "} <
+        div className = "item-copyright" > { " " } { String.fromCharCode(169) } { new Date().getFullYear() }
+        Lisha.All rights reserved. { " " } <
+        /div>{" "} <
+        /footer>
+    );
 }
 
 export default Footer;
